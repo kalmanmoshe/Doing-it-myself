27c2e04a89a976175cb1c337cc900481f732ed3b
kalmanmoshe <moshe196713@gmail.com>
2024-09-29 00:33:10 +0300

    modified:   main.js 	modified:   main.ts 	modified:   mathEngine.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b106bed703733e30b9bcbf05e539205d57f3758f8c4cd4dd972a030b57069f5e
-size 9186
+oid sha256:2128f4bbd3e0f4bf5143f9dcd17f3e8aeeada26c8d077efb7a441d482f817f19
+size 9051
