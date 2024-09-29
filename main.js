fd033727d9b050c74f7e0abba0249ed8569e58de
משה קלמן ויינשטוק <moshe@mshs-Laptop.local>
2024-09-29 22:13:20 +0300

    your commit message

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2128f4bbd3e0f4bf5143f9dcd17f3e8aeeada26c8d077efb7a441d482f817f19
-size 9051
+oid sha256:624af82948ac47ef3fe309c46be4bdace17a9924700c7239f147b079bc0376e6
+size 9980
