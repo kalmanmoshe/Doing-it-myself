bfa46b042eb4ecf918a2ca9f2cc4f010652c2a78
kalmanmoshe <moshe196713@gmail.com>
2024-11-04 21:26:00 +0200

    new file:   .eslintignore 	new file:   .eslintrc#	modified:   data.json 	new file:   dist/data.json 	new file:   "dist/main-\342\201\250\327\236\327\251\327\224\342\201\251\342\200\231s \342\201\250Laptop\342\201\251.js"	modified:   dist/main.js 	new file:   dist/src/desplyModals.js 	new file:   dist/src/imVeryLazy.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2b840f1d94060cf2c2fcda824d61768d74618917552c0ac316ddad1a23b6577
-size 31087
+oid sha256:d0eaa8d08a41b949b1c7313bce519f0b9ddd040d94ea0d83731bf70e815ff47c
+size 7953466
