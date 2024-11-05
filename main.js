e262e0baedc5fb33259950b30e1f238ef3ad6a37
kalmanmoshe <moshe196713@gmail.com>
2024-11-05 11:27:18 +0200

    modified:   main.js 	modified:   manifest.json 	modified:   versions.json

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ba31191eca4cfdc9653af1676ddba9af285bd04c1cf456da2be9f90445b5df8
+oid sha256:9feeac06695ac4b63239cd3357a688dfbc77665be125a895864a7ada5ad0ae72
 size 7953567
