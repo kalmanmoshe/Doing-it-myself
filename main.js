983a560f8e3b427dc0d63f113db08c36732acda4
Kalmanmoshe <moshe196713@gmail.com>
2024-11-12 19:37:15 +0200

    modified:   dist/src/main.js 	modified:   dist/src/tikzjax/tikzjax.js 	modified:   main.js 	modified:   src/main.ts 	modified:   src/tikzjax/tikzjax.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7d57c5abbfb047cd53be46b52a6646025fc5a02275bfb8675500ced947fdad7
-size 8131629
+oid sha256:4f62e3b3f68868978bbbb800777228cc4fbbd6a4ee912318bda89f847a2116d1
+size 8132945
