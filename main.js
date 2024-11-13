1092ac93f482ec88b56b2e1ccf4850ab77439f88
kalmanmoshe <moshe196713@gmail.com>
2024-11-13 12:39:13 +0200

    modified:   dist/src/imVeryLazy.js 	modified:   dist/src/main.js 	modified:   dist/src/mathEngine.js 	modified:   dist/src/mathUtilities.js 	modified:   dist/src/tempCodeRunnerFile.js 	modified:   dist/src/tikzjax/tempCodeRunnerFile.js 	modified:   dist/src/tikzjax/tikzjax.js 	modified:   esbuild.config.mjs 	modified:   main.js 	modified:   node_modules/.bin/acorn

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f62e3b3f68868978bbbb800777228cc4fbbd6a4ee912318bda89f847a2116d1
-size 8132945
+oid sha256:c8ca362a467ef85d800b482e428ca32fffb6bf4c22684cd85081466a417a2e5e
+size 8131000
