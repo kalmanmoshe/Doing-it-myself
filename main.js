ce3d00cc54086deaca569731808d10f84c3989a3
Kalmanmoshe <moshe196713@gmail.com>
2024-11-13 16:32:04 +0200

    modified:   auto-commit.ps1 	modified:   main.js 	modified:   src/tikzjax/tikzjax.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a04679aece33244946ace90b2d977996053eedc002af3677580331661102410
+oid sha256:edd77a9a3a99fc83bd620e3eb90db41b46198df50700dd83b9c9217ca797b6fc
 size 8132527
