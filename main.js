e4a908021de04dfe179a1102c7378989c54f8933
Kalmanmoshe <moshe196713@gmail.com>
2024-11-21 20:07:15 +0200

    modified:   dist/src/imVeryLazy.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33f563fe40ce11ea8557fb817c0a79b1199621eca2126948ba91eab4c6153e5e
-size 8142334
+oid sha256:37c3b11cc39bfde40fd9df8b2b92f17be92e081efd838dfeb5c04416dab20352
+size 8141788
