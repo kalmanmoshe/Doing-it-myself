523560e420c36cbff8b19e96053c269fa0aab300
kalmanmoshe <moshe196713@gmail.com>
2024-12-03 12:00:58 +0200

    modified:   dist/src/imVeryLazy.js 	modified:   dist/src/main.js 	modified:   dist/src/mathEngine.js 	modified:   dist/src/mathUtilities.js 	modified:   dist/src/temp.js 	modified:   dist/src/tikzjax/myTikz.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c06a6d14df4cfc5abdc349a07c11f066e126f9b2b5363bf4ba9da8c392fe3df4
-size 8154292
+oid sha256:17869effbe65ab51b3ba8b097ba320721082699062cc34205cbdeb56eb3e717e
+size 8154858
