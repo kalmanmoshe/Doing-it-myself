452362965c09ee2ddbfea90c6d47cb1ab05aefec
kalmanmoshe <moshe196713@gmail.com>
2024-12-10 20:45:09 +0200

    modified:   dist/src/tikzjax/interpret/tokenizeTikzjax.js 	modified:   main.js 	modified:   src/tikzjax/interpret/tokenizeTikzjax.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:166c4be994255a19981e7dbc2cb9d79238dc1192a1877608006b21a94e1cfc29
-size 8158728
+oid sha256:62bfc760dc7c72c60de0d02ce62345cae2cc624b2bc97d44d21f3b33cc142ef5
+size 8158792
