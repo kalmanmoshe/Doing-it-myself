9d3208dcb19b44b17c227fdbf0a18c796cf97937
Kalmanmoshe <moshe196713@gmail.com>
2024-12-15 21:40:46 +0200

    modified:   dist/src/codemirror/config.js 	modified:   dist/src/codemirror/history.js 	modified:   dist/src/codemirror/snippet_change_spec.js 	modified:   dist/src/codemirror/snippet_queue_state_field.js 	modified:   dist/src/codemirror/tabstops_state_field.js 	modified:   dist/src/editor utilities/context.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59aa9602d23c6b8347f3cbdb4eb6f0676a587f25191352d0ba32d5dcfc949c0c
-size 8324611
+oid sha256:7b3fd12ee0ed24026e2ed5a1f2bfd2c025103abf963c5856a118bff5c24a68a0
+size 8324678
