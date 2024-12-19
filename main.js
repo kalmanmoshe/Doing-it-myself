2068c23acb90cbe52a6c9cedfb8a3147c7c11bd4
Kalmanmoshe <moshe196713@gmail.com>
2024-12-19 14:35:20 +0200

    modified:   dist/src/setEditorExtensions.js 	modified:   dist/src/snippets/options.js 	modified:   dist/src/utils/context.js 	modified:   main.js 	modified:   src/setEditorExtensions.ts 	modified:   src/snippets/options.ts 	modified:   src/utils/context.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6e10152a09cdc7b0b8ff051f013a9b8e079a317d97d4b6636723072abb053f9
-size 8942985
+oid sha256:4f37c271f2d9ed94eb2f3dc283b406d45a91b4b58ceeaf512a39f665269bcf0a
+size 8942961
