c92741168617deace79e8ad9fbd221092b37cfa6
kalmanmoshe <moshe196713@gmail.com>
2024-12-21 16:46:42 +0200

    modified:   dist/src/desplyModals.js 	modified:   dist/src/imVeryLazy.js 	modified:   dist/src/main.js 	new file:   dist/src/mathParser/mathEngine.js 	new file:   dist/src/mathParser/mathUtilities.js 	modified:   dist/src/tikzjax/myTikz.js 	modified:   dist/src/tikzjax/tikzjax.js 	modified:   dist/src/utils/dataManager.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f65fd100ba15ea8b2ab5719e0f710b63b3f4b624dc1ff3cf479351659208e3fc
-size 8954257
+oid sha256:b6af26d5cc71c5173cc59fcb1a66d83a76fd78158484111287655bbfe4d2eb52
+size 8958122
