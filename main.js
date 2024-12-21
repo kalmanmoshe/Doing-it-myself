35c733b0b175bbb26ed74c965b7682e14870e8a4
kalmanmoshe <moshe196713@gmail.com>
2024-12-21 15:56:06 +0200

    modified:   dist/src/mathEngine.js 	modified:   dist/src/tikzjax/interpret/tempCodeRunnerFile.js 	modified:   dist/src/tikzjax/interpret/tokenizeTikzjax.js 	modified:   dist/src/tikzjax/tempCodeRunnerFile.js 	modified:   dist/src/tikzjax/tikzjax.js 	new file:   dist/src/utils/dataManager.js 	modified:   dist/src/utils/staticData.js 	modified:   main.js 	renamed:    src/mathEngine.js -> src/mathEngine.ts 	modified:   src/tikzjax/interpret/tokenizeTikzjax.ts 	modified:   src/tikzjax/tikzjax.ts 	modified:   src/utils/dataManager.ts 	modified:   src/utils/staticData.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcf5ef2ad43cf1d4ef5f1142618a1afec6d96040f5a43199107588f0765514cd
-size 8952664
+oid sha256:f65fd100ba15ea8b2ab5719e0f710b63b3f4b624dc1ff3cf479351659208e3fc
+size 8954257
