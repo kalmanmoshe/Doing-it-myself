a12fd2e728c5b6190c089aed24bdb5055ecc2902
kalmanmoshe <moshe196713@gmail.com>
2024-12-22 01:05:27 +0200

    modified:   dist/src/mathParser/mathEngine.js 	modified:   main.js 	modified:   src/mathParser/mathEngine.ts

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4b2716cd819a6ec8d842d9d785d1b471f53fff57919d99739eb548c12e94f51
-size 8131597
+oid sha256:c3eb1b3375f8c78e1763358d8720daeee80e8d56d409347c6e2747cb1ac5a1da
+size 8959382
