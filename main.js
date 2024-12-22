3ce5e7780c4240d723bcb1fc4c216d2b01c74e42
Kalmanmoshe <moshe196713@gmail.com>
2024-12-22 14:53:52 +0200

    modified:   dist/src/editor utilities/context.js 	modified:   dist/src/editor utilities/editor_utils.js 	modified:   dist/src/editor utilities/options.js 	modified:   dist/src/editorDecorations.js 	modified:   dist/src/editor_extensions/conceal.js 	modified:   dist/src/editor_extensions/conceal_fns.js 	modified:   dist/src/editor_extensions/conceal_maps.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3eb1b3375f8c78e1763358d8720daeee80e8d56d409347c6e2747cb1ac5a1da
-size 8959382
+oid sha256:792b0f42546ed71398d65a9e4cccefce2b72d45c3a8e0a9486fb4e341bfc4335
+size 8951736
