4be001a820f439edce940d748bc2c722e534b98b
Kalmanmoshe <moshe196713@gmail.com>
2024-12-23 19:46:32 +0200

    modified:   dist/src/editor utilities/context.js 	modified:   dist/src/editor utilities/editor_utils.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b48cea153b4aeabafbc80c1701029f298b40683e49c912c8f69bd39d24b1c167
-size 8951831
+oid sha256:a3666210020df4a60a188871e39aa5694c8c40a735a2b4b0e41c09a81bceb7ef
+size 8950021
