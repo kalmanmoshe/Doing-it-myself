ceef614fe9501fbfaab7f751b1c2c3399c01bc55
kalmanmoshe <moshe196713@gmail.com>
2025-01-02 16:07:24 +0200

    modified:   dist/src/mathParser/mathEngine.js 	modified:   dist/src/mathParser/mathJaxTokens.js 	modified:   dist/src/mathParser/tempCodeRunnerFile.js 	modified:   dist/src/utils/staticData.js 	modified:   main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccd3817fdd4820cde06a8836ad675181a42acb5d7c20b074e5f4b35da3781492
-size 8956360
+oid sha256:a2bd1b6d37c3cff857333eac7fa5d5d4cd15294ed29b40e60655a8dc50de8716
+size 8960838
