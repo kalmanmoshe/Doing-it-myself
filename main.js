353b3a5c11692c31237a78c9a39ca933f43b5b11
Kalmanmoshe <moshe196713@gmail.com>
2025-01-06 21:45:52 +0200

    modified:   dist/src/mathParser/mathJaxTokens.js 	modified:   main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:821661218996ad521a858d48c98d02318da66defc1befa11a214a4115e8fa6cc
-size 8967929
+oid sha256:ce2286b96d3e0ef69eab633cde8773b1d07223b7a4d0ed35f57af3b1b8402377
+size 8968381
