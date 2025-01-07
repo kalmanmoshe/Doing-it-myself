faea1ebff81ba51e2ebeb8512a838ae2ee5d51c3
Kalmanmoshe <moshe196713@gmail.com>
2025-01-07 22:38:44 +0200

    modified:   dist/src/editor utilities/context.js 	modified:   dist/src/editor utilities/editor_utils.js 	modified:   dist/src/editor utilities/options.js 	modified:   dist/src/editorDecorations.js 	modified:   dist/src/editor_extensions/conceal.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27767672cd09ea9174d84cc5fcb9a5d0972882436d9ec288555d16dfb70a609e
-size 8970519
+oid sha256:f1a938fbd539abdc15050ebfefa4b8b701a0f611c05a43d5d224d9c75f0507cd
+size 8969350
