1d8cd19964ff46213310ce91d532788fa7c8f9a7
kalmanmoshe <moshe196713@gmail.com>
2025-01-13 22:41:37 +0200

    modified:   dist/src/basicToken.js 	modified:   dist/src/editor utilities/context.js 	modified:   dist/src/editor utilities/editor_utils.js 	modified:   dist/src/editor utilities/options.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf67d47dbfb27b17dacf1e3581cd6539c26d6898d5d4ca061a9d19f7d1e7e02d
-size 9126237
+oid sha256:14cdc48c9a0963749a887194db3f6f8cdd80fe614eda38a57f686c5bd84ef9a6
+size 9126489
