6069881ed4abe73cff2958a70ae26658de348b98
kalmanmoshe <moshe196713@gmail.com>
2025-01-14 07:05:08 +0200

    modified:   dist/src/latexRender/PdfTeXEngine.js 	new file:   dist/src/latexRender/main.js 	modified:   dist/src/latexRender/pdftocairo.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14cdc48c9a0963749a887194db3f6f8cdd80fe614eda38a57f686c5bd84ef9a6
-size 9126489
+oid sha256:969d60b2e985724e2a5e8658b70789fdc5718bf6dfffca716f18eeffbbc75240
+size 43733667
