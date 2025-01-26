429e14b8a36c24076c0d13902447b2fc99088dc6
Kalmanmoshe <moshe196713@gmail.com>
2025-01-26 21:26:46 +0200

    modified:   dist/src/ inputMonitors.js 	modified:   dist/src/basicToken.js 	modified:   dist/src/editorDecorations.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d6d29abe1b0417b97fa8a4e86f376d67e74de1d6f76c16cbc94592a047e0dc
-size 9126938
+oid sha256:662c0c45e47717397af981d51e62dde9bbffab73edafb7296eb5cdc1b9126ab7
+size 9123857
