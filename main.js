25ff7c265c1aab6140a5a5704040879ae7ede9dc
kalmanmoshe <moshe196713@gmail.com>
2025-01-26 08:11:06 +0200

    Update basicToken.js with recent modifications

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b34ab3d9e874ee0d74c2987712da19df4a333567cfc7b6be1cc7ce254a05cb7a
-size 9126820
+oid sha256:80d6d29abe1b0417b97fa8a4e86f376d67e74de1d6f76c16cbc94592a047e0dc
+size 9126938
