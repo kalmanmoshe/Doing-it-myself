da22d8f5ef81c4b4afb28dbd367c6427dc9f1286
kalmanmoshe <moshe196713@gmail.com>
2025-01-28 12:37:03 +0200

    modified:   data.json 	modified:   dist/data.json 	modified:   dist/src/ inputMonitors.js 	modified:   dist/src/basicToken.js 	modified:   dist/src/desplyModals.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:662c0c45e47717397af981d51e62dde9bbffab73edafb7296eb5cdc1b9126ab7
-size 9123857
+oid sha256:caed67aacc8ac8de7ffc638ebe2238f1a239736f0d6eb9688c755daf8f458d1b
+size 9119641
