c57195c19889a9b0da131ece54c8d86053fc847c
kalmanmoshe <moshe196713@gmail.com>
2025-01-31 21:42:53 +0200

    deleted:    Doing-it-myself.code-workspace 	modified:   data.json 	deleted:    dist/data.json 	deleted:    dist/main-DESKTOP-CB8P7KD.js 	deleted:    "dist/main-\342\201\250\327\236\327\251\327\224\342\201\251\342\200\231s \342\201\250Laptop\342\201\251.js" 	deleted:    dist/main.js 	deleted:    dist/mathEngine-DESKTOP-CB8P7KD.js 	deleted:    dist/mathEngine.js 	deleted:    dist/src/ inputMonitors.js 	deleted:    dist/src/.DS_Store 	deleted:    dist/src/basicToken.js 	deleted:    dist/src/codemirror/config.js 	deleted:    dist/src/codemirror/history.js 	deleted:    dist/src/codemirror/snippet_change_spec.js 	deleted:    dist/src/codemirror/snippet_queue_state_field.js 	deleted:    dist/src/codemirror/tabstops_state_field.js 	deleted:    dist/src/desplyModals.js 	deleted:    dist/src/editor utilities/context.js 	deleted:    dist/src/editor utilities/editor_utils.js 	deleted:    dist/src/editor utilities/options.js 	deleted:    dist/src/editor utilities/tempCodeRunnerFile.js 	deleted:    dist/src/editorDecorations.js 	deleted:    dist/src/editorExtensions.js 	deleted:    dist/src/editor_extensions/conceal.js 	deleted:    dist/src/editor_extensions/conceal_fns.js 	deleted:    dist/src/editor_extensions/conceal_maps.js 	deleted:    dist/src/editor_extensions/highlight_brackets.js 	deleted:    dist/src/editor_extensions/math_tooltip.js 	deleted:    dist/src/features/auto_enlarge_brackets.js 	deleted:    dist/src/features/autofraction.js 	deleted:    dist/src/features/editor_commands.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caed67aacc8ac8de7ffc638ebe2238f1a239736f0d6eb9688c755daf8f458d1b
-size 9119641
+oid sha256:e1828c1a9953fb779e4ae6ce77c24d78dbc8208757ad3c840985dca01a3e9896
+size 109836290
