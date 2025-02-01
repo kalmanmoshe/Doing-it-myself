9743ca1f07d7f297acb6a9326a20bd43b6f712ca
kalmanmoshe <moshe196713@gmail.com>
2025-02-01 23:34:35 +0200

    deleted:    auto-commit.ps1 	modified:   data.json 	modified:   esbuild.config.mjs 	deleted:    m.svg 	modified:   main.js 	new file:   node_modules/.bin/browserslist 	new file:   node_modules/.bin/browserslist.cmd 	new file:   node_modules/.bin/browserslist.ps1 	new file:   node_modules/.bin/cssesc 	new file:   node_modules/.bin/cssesc.cmd 	renamed:    node_modules/js-beautify/node_modules/.bin/glob -> node_modules/.bin/glob 	renamed:    node_modules/js-beautify/node_modules/.bin/glob.cmd -> node_modules/.bin/glob.cmd 	renamed:    node_modules/js-beautify/node_modules/.bin/glob.ps1 -> node_modules/.bin/glob.ps1 	new file:   node_modules/.bin/jsesc 	new file:   node_modules/.bin/jsesc.cmd     new file:   node_modules/.bin/jsesc.ps1 	new file:   node_modules/.bin/json5 	new file:   node_modules/.bin/json5.cmd 	new file:   node_modules/.bin/json5.ps1 	new file:   node_modules/.bin/parser 	new file:   node_modules/.bin/parser.cmd 	new file:   node_modules/.bin/parser.ps1 	new file:   node_modules/.bin/prettier 	new file:   node_modules/.bin/prettier.cmd 	new file:   node_modules/.bin/prettier.ps1 	new file:   node_modules/.bin/update-browserslist-db 	new file:   node_modules/.bin/update-browserslist-db.cmd 	new file:   node_modules/.bin/update-browserslist-db.ps1 	modified:   node_modules/.cache/esbuild-plugin-inline-worker/swiftlatexpdftex.worker.js 	new file:   node_modules/.cache/esbuild-plugin-inline-worker/try.worker.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/@ampproject/remapping/LICENSE 	new file:   node_modules/@ampproject/remapping/README.md 	new file:   node_modules/@ampproject/remapping/dist/remapping.mjs 	new file:   node_modules/@ampproject/remapping/dist/remapping.mjs.map 	new file:   node_modules/@ampproject/remapping/dist/remapping.umd.js 	new file:   node_modules/@ampproject/remapping/dist/remapping.umd.js.map 	new file:   node_modules/@ampproject/remapping/dist/types/build-source-map-tree.d.ts 	new file:   node_modules/@ampproject/remapping/dist/types/remapping.d.ts 	new file:   node_modules/@ampproject/remapping/dist/types/source-map-tree.d.ts 	new file:   node_modules/@ampproject/remapping/dist/types/source-map.d.ts 	new file:   node_modules/@ampproject/remapping/dist/types/types.d.ts 	new file:   node_modules/@ampproject/remapping/package.json 	new file:   node_modules/@babel/code-frame/LICENSE 	new file:   node_modules/@babel/code-frame/README.md     new file:   node_modules/@babel/code-frame/lib/index.js 	new file:   node_modules/@babel/code-frame/lib/index.js.map 	new file:   node_modules/@babel/code-frame/package.json 	new file:   node_modules/@babel/compat-data/LICENSE 	new file:   node_modules/@babel/compat-data/README.md 	new file:   node_modules/@babel/compat-data/corejs2-built-ins.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1828c1a9953fb779e4ae6ce77c24d78dbc8208757ad3c840985dca01a3e9896
-size 109836290
+oid sha256:f56ce9a96ab877f4a3cefeb4c80cf92640bd35707c6024c06e07b4a8a767267f
+size 19502341
